8e0d09774c34db22ce0b556046d9c5c8a735bb72
Lewis Brennan <lewisbrennan444@gmail.com>
2023-06-01 10:26:20 +0100

    initial commit

---
+++ courseBooking/src/test/java/com/courseBooking/courseBooking/CourseBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courseBooking.courseBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
